f17e2c09fcf6dc75b202984fc71a683e69ff8c0c
Seungmin Cho <smin8475@gmail.com>
2024-12-08 23:58:47 -0600

    Portfolio 2

--- game.hpp
+++ game.hpp
@@ -1,26 +1,33 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include "board.hpp"
 
 using namespace std;
 
-class Game
-{
-    private:
-    public:
-        Board board;
-        string input;
-        int validInput;
-        int counter;
-        Game();
-        void start();
-        bool winChecker(char symbol);
-        bool validateInput();
-        char currentPlayer;
-        void switchPlayer();
-        void promptMove();
-        bool playAgain();
-        
-        
+class Game {
+private:
+    Board board;
+    char currentPlayer;
+    string input;
+    int validInput;
+    int counter;
+    bool isBattleMode;
+    string player1Archetype;
+    string player2Archetype;
+    bool player1UsedSpecial;
+    bool player2UsedSpecial;
+
+    void selectArchetypes();
+    void handleBattleMove();
+
+public:
+    Game(bool battleMode = false);
+    void start();
+    bool winChecker(char symbol);
+    bool validateInput();
+    void switchPlayer();
+    void promptMove();
+    bool playAgain();
 };
